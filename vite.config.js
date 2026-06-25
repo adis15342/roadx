9a409e474cf599bf505ce1ea30cd6a33412873d9
gargyogesh1 <106339179+gargyogesh1@users.noreply.github.com>
2026-06-25 14:52:14 +0530

    Fix PWA cache size limit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'],
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    })
+  ]
 })
